029490df1f5daa5cbad8c173a6a1f3a560f65974
Augusto Forero <a.forarc@gmail.com>
2021-12-13 20:21:30 -0500

    Add logs

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
+const { v4: uuidv4 } = require('uuid');
 
 const api = require('./api/v1');
+const logger = require('./config/logger');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('X-Request-Id', uuidv4());
+
+  next();
+});
+
+// prettier-ignore
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.info(message),
+    },
+  }),
+);
+
+app.use(express.json());
 
 app.use('/api', api);
 
 app.use((req, res, next) => {
   const statusCode = 404;
   const message = 'Error. Route not found';
+
+  logger.warn(message);
 
   next({
     statusCode,
@@ -18,6 +40,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = '' } = err;
+
+  logger.error(message);
 
   res.status(statusCode);
   res.json({
