dbf5a584502574884afb8ee4ca07514b5029081a
Augusto Forero <a.forarc@gmail.com>
2021-12-13 22:06:38 -0500

    Update Users schema

--- server/api/v1/users/model.js
+++ server/api/v1/users/model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { hash, compare } = require('bcryptjs');
 
 const fields = {
   firstName: {
@@ -28,9 +29,53 @@ const fields = {
   },
 };
 
+const hiddenFields = ['password'];
+
 const user = new mongoose.Schema(fields, {
   timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+  toObject: {
+    virtuals: true,
+  },
 });
+
+user
+  .virtual('fullname')
+  .get(function getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  })
+  .set(function setFullName(value) {
+    const [firstName = '', lastName = ''] = value.split(' ');
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+  });
+
+user.methods.toJSON = function toJSON() {
+  const document = this.toObject();
+
+  hiddenFields.forEach((field) => {
+    if (document[field] !== undefined) {
+      delete document[field];
+    }
+  });
+
+  return document;
+};
+
+user.pre('save', async function save(next) {
+  if (this.isNew || this.isModified('password')) {
+    this.password = await hash(this.password, 10);
+  }
+
+  next();
+});
+
+user.methods.verifyPassword = function verifyPassword(value) {
+  return compare(value, this.password);
+};
 
 const model = mongoose.model('user', user);
 
