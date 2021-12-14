b393995f073d2bbacfeacddc1397e3c8d56ad181
Augusto Forero <a.forarc@gmail.com>
2021-12-13 20:57:46 -0500

    Add mongoose and connect to the database

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const http = require('http');
 
 const app = require('./server');
 const config = require('./server/config');
+const { connect } = require('./server/database');
 
 const hostname = '127.0.0.1';
-const { port } = config;
+const { port, database } = config;
+
+connect({
+  url: database.url,
+  username: database.username,
+  password: database.password,
+});
 
 const server = http.createServer(app);
 
---
+++ server/database.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { logger } = require('./config/logger');
+
+exports.connect = ({ url = '', username, password }, options = {}) => {
+  let dburl = '';
+
+  if (username && password) {
+    dburl = `mongodb://${username}:${password}@${url}`;
+  } else {
+    dburl = `mongodb://${url}`;
+  }
+
+  mongoose.connect(dburl, {
+    ...options,
+  });
+
+  mongoose.connection.on('connected', () => {
+    logger.info('Database connected');
+  });
+
+  mongoose.connection.on('close', () => {
+    logger.info('Database disconnected');
+  });
+
+  mongoose.connection.on('error', (error) => {
+    logger.error(`Database error: ${error}`);
+  });
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      logger.info('Database disconnected due to app termination');
+      process.exit(0);
+    });
+  });
+};
+
+exports.disconnected = () => {
+  mongoose.connection.close(() => {
+    logger.info('Database disconnected successfully');
+    process.exit(0);
+  });
+};
