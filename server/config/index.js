98c5fe2903034e78f65a8f2a001abd34709295ee
Jerson Zúñiga-Coayla <jzunigacoayla@gmail.com>
2022-02-07 07:34:42 -0500

    feat: add database environment variables

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 4000,
+  database: {
+    url: process.env.DATABASE_URL,
+    protocol: process.env.DATABASE_PROTOCOL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
   pagination: {
     page: 1,
     limit: 10,
